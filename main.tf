3a51e75aab1eae2d276312762cc29ae05b0a451b
Emanuel Nunes <costaemanuel766@gmail.com>
2025-04-05 19:59:08 -0300

    alteracao regiao  bucket

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "google_storage_bucket" "my-bucket" {
   name                     = "ci-cd-bucket-tf-github"
   project                  = "crypto-resolver-451921-d3"
-  location                 = "US"
+  location                 = "EU"
   force_destroy            = true
   public_access_prevention = "enforced"
 }
